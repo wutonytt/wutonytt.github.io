da559bfddfd8f4ba8d246917948682a5cbe01f67
Tony Wu <wutonytt@gmail.com>
2023-06-08 21:44:45 +0800

    feat: add Skills component

---
+++ src/components/Skills.tsx
@@ -0,0 +1,61 @@
+import { Card, Container, Grid, Text } from "@nextui-org/react";
+import { skills } from "../data/skills";
+
+const Skills = () => {
+  return (
+    <Container gap={2}>
+      <Text id="skills" h3>
+        Skills
+      </Text>
+      <Grid.Container gap={2} css={{ textIndent: "1em" }}>
+        <Grid xs={4}>
+          <Card>
+            <Card.Header>
+              <Text b>Programming Languages</Text>
+            </Card.Header>
+            <Card.Divider />
+            <Card.Body>
+              {skills.programming_languages.map((lang) => (
+                <Text h5 color="gray">
+                  - {lang}
+                </Text>
+              ))}
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={4}>
+          <Card>
+            <Card.Header>
+              <Text b>Tools</Text>
+            </Card.Header>
+            <Card.Divider />
+            <Card.Body>
+              {skills.tools.map((tool) => (
+                <Text h5 color="gray">
+                  - {tool}
+                </Text>
+              ))}
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={4}>
+          <Card>
+            <Card.Header>
+              <Text b>Frameworks</Text>
+            </Card.Header>
+            <Card.Divider />
+            <Card.Body>
+              {skills.frameworks.map((framework) => (
+                <Text h5 color="gray">
+                  - {framework}
+                </Text>
+              ))}
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
+    </Container>
+  );
+};
+
+export default Skills;
---
+++ src/data/skills.ts
@@ -0,0 +1,14 @@
+export const skills = {
+  programming_languages: [
+    "Python",
+    "JavaScript",
+    "TypeScript",
+    "C++",
+    "Java",
+    "SQL",
+    "MATLAB",
+    "Go",
+  ],
+  tools: ["Git", "MySQL", "PostgreSQL", "MongoDB", "AWS", "Azure"],
+  frameworks: ["Node.js", "FastAPI", "Vue.js", "NextUI"],
+};
