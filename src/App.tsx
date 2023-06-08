06931ba39671e7ac673c36748e129dc9594984fd
Tony Wu <wutonytt@gmail.com>
2023-06-08 21:43:23 +0800

    feat: add Projects component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,70 @@
 import "./App.css";
-import { NextUIProvider, Container, Spacer } from "@nextui-org/react";
+import {
+  NextUIProvider,
+  Container,
+  Spacer,
+  createTheme,
+} from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import Intro from "./components/Intro";
 import AboutMe from "./components/AboutMe";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
 import Navigator from "./components/Navigator";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+const lightTheme = createTheme({
+  type: "light",
+  theme: {
+    colors: {
+      titleColor: "#24397A",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {
+    colors: {
+      titleColor: "#FFFFFF",
+    },
+  },
+});
 
 function App() {
   return (
-    <NextUIProvider>
-      {
-        <>
-          <Navigator />
-          <Container gap={8}>
-            <Spacer y={2} />
-            <Intro />
-            <Spacer y={2} />
-            <AboutMe />
-            <Spacer y={2} />
-            <Education />
-            <Spacer y={2} />
-            <Experience />
-            <Spacer y={2} />
-          </Container>
-        </>
-      }
-    </NextUIProvider>
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        {
+          <>
+            <Navigator />
+            <Container gap={8}>
+              <Spacer y={6} />
+              <Intro />
+              <Spacer y={6} />
+              <AboutMe />
+              <Spacer y={4} />
+              <Education />
+              <Spacer y={4} />
+              <Experience />
+              <Spacer y={4} />
+              <Projects />
+              <Spacer y={4} />
+              <Skills />
+              <Spacer y={4} />
+            </Container>
+          </>
+        }
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
 
